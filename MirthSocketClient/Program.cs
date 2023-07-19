cd2f2b88144aa49af4494c8b318a60df934637bd
Lukas Emmerich <lukas.emmerich@healex.systems>
2023-07-19 14:07:14 +0200

    feat: Basic Socket writing client

---
+++ MirthSocketClient/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Utility;
+
+namespace MirthSocketClient;
+
+class Program
+{
+    public static void Main()
+    {
+        var messages = GetMessages(1);
+        // setup
+        var ipAddr = IPAddress.Parse("127.0.0.1");
+        var ipEndpoint = new IPEndPoint(ipAddr, 16661);
+        using Socket client = new(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        client.ConnectAsync(ipEndpoint);
+
+        foreach (var message in messages)
+        {
+            // convert message to binary
+            var binaryMessage = message.Pack(false, true);
+            // Console.WriteLine(binaryMessage.HexDump());
+            // send
+            _ = client.SendAsync(binaryMessage,SocketFlags.None).Result;
+            
+            // wait for ack
+            var responseBuffer = new byte[1024];
+            var received = client.ReceiveAsync(responseBuffer, SocketFlags.None).Result;
+            var rawResponse = new byte[received];
+            Array.Copy(responseBuffer, 0, rawResponse, 0, received);
+            Console.WriteLine($"Received response of length: {received}");
+            Console.WriteLine(rawResponse.CheckAck());
+            
+            // ack ack
+            var ack = new byte[4] { 0x0b, 0x06, 0x1c, 0x0d };
+            _ = client.SendAsync(ack, SocketFlags.None);
+            Thread.Sleep(1000);
+        }
+        
+    }
+
+    private static List<string> GetMessages(int number)
+    {
+        List<string> result = new List<string>();
+        for(var i = 0; i<number; i++)
+        {
+            var paddedNumber = i.ToString().PadLeft(6, '0');
+            var message = $"MSH|^~\\&|x|{paddedNumber}|CERNER|HOSPITAL-A|201401291848||ADT^A01|1912340911|P|2.3|||AL|NE|\r\n" +
+                          "EVN|A01|201401291848|||REJKB1\r\n" +
+                          "PID||ABC123|987654|ALT789|PETTY^TOM^^^^||19781218|M||2106-3|10144 MAPLE AVE^^IRVINE^CA^92614||(949)123-1234|||||0053820452|220675537||AME||||1|||||\r\n" +
+                          "PV1||I|S-2302-1^S-2302^A|C|||1111111^PINA|||SUR|||||A0||1111111^PINA|S||S|P||||||||||||||||||IAH|||||201401291848|\r\n" +
+                          "PV2||D|42.41^Partial esophagectomy^I9|||||201401290900|201401310900|3|3|||||||||||||||||||||||||a|";
+
+            result.Add(message);
+        }
+
+        return result;
+    }
+}
